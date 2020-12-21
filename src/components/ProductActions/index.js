1bab2f8ea5cd136e0a1a5b3884b1c1cc0055c361
alzateja <alzateja@gmail.com>
2020-12-21 02:47:18 -0500

    switch our app to use our Rescript ProductActions

--- src/components/ProductActions/index.js
+++ src/components/ProductActions/index.js
@@ -1,3 +1,7 @@
-import ProductActions from "./ProductActions";
+// Our old import of ProductActions
+// import ProductActions from "./ProductActions";
+
+// Our import of generated ProductActions
+import {make as ProductActions} from "./ProductActions.bs"
 
 export default ProductActions;
