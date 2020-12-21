77c3aa72856e0b22500450eb555eb7630c59a8bb
alzateja <alzateja@gmail.com>
2020-12-21 02:00:48 -0500

    Add example of gentype declarations

---
+++ src/examples/ExampleType/ExampleType.gen.tsx
@@ -0,0 +1,12 @@
+/* TypeScript file generated from ExampleType.res by genType. */
+/* eslint-disable import/first */
+
+
+// tslint:disable-next-line:no-var-requires
+const ExampleTypeBS = require('./ExampleType.bs');
+
+export const stringVariable: string = ExampleTypeBS.stringVariable;
+
+export const integerVariable: number = ExampleTypeBS.integerVariable;
+
+export const floatVariable: number = ExampleTypeBS.floatVariable;
--- src/examples/ExampleType/ExampleType.res
+++ src/examples/ExampleType/ExampleType.res
@@ -1,5 +1,8 @@
+@gentype
 let stringVariable = "Hello " ++ "James"
 
+@gentype
 let integerVariable = 12 + 12
 
+@gentype
 let floatVariable = 12.1 +. 12.5
